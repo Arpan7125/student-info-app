207d6330516f232064007f998ad695ab7b435f0d
Arpan Mukherjee <125454700+Arpan7125@users.noreply.github.com>
2026-09-07 21:09:56 +0530

    Initial student information app

---
+++ README.md
@@ -0,0 +1,4 @@
+# student-info-app
+
+A very simple Student Information web application used for a collaborative
+Git workflow lab exercise.
---
+++ script.js
@@ -0,0 +1,3 @@
+// Student Information System
+// JavaScript functionality will be added in the feature/javascript branch.
+console.log("Student Information System loaded");
